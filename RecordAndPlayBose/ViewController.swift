3b4d23bf15b581ce0e7f9232fdaa4ace1ea5c268
Armando Gonzalez <mando@Macboook.local>
2019-03-11 15:44:53 -0400

    UI updates and refactoring

--- RecordAndPlayBose/ViewController.swift
+++ RecordAndPlayBose/ViewController.swift
@@ -11,12 +11,15 @@ import AVFoundation
 import CoreBluetooth
 
 class ViewController: UIViewController {
-    let songKeyPath = "currentSong"
     let audioRecordingFileName = "audio_recordAndPlayApp.m4a"
-
+    let viewBackgroundColor = UIColor(red: 3/255.0, green: 23/255.0, blue: 37/255.0, alpha: 1.0)
+    let butttonBackgroundColor = UIColor(red: 59/255.0, green: 101/255.0, blue: 126/255.0, alpha: 1.0)
+    
+    var boseLogo: UIImageView!
+    
     var playButton: UIButton!
     var recordButton: UIButton!
-    var stackView: UIStackView!
+    var buttonsStackView: UIStackView!
     
     var audioSession: AVAudioSession!
     var audioRecorder: AVAudioRecorder!
@@ -35,29 +38,38 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         initialSetup()
-        setupStackView()
+        setupButtonsStackView()
         setupAudioRecorder()
     }
     
     private func initialSetup() {
-        view.backgroundColor = .white
-    }
-
-    private func setupStackView() {
-        setupButtons()
-        stackView = UIStackView(arrangedSubviews: [playButton,recordButton])
-        view.addSubview(stackView)
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        stackView.axis = .horizontal
-        stackView.spacing = 25
-        stackView.distribution = .fill
-        let centerX = NSLayoutConstraint(item: stackView, attribute: .centerX, relatedBy: .equal, toItem: view, attribute: .centerX, multiplier: 1, constant: 0)
-        let centerY = NSLayoutConstraint(item: stackView, attribute: .centerY, relatedBy: .equal, toItem: view, attribute: .centerY, multiplier: 1, constant: 0)
-        view.addConstraints([centerX,centerY])
+        view.backgroundColor = viewBackgroundColor
+        setupLogo()
     }
     
-    private func addCenterConstraints(for sv: UIStackView) {
-        
+    private func setupLogo() {
+        boseLogo = UIImageView(image: UIImage(named: "bose"))
+        view.addSubview(boseLogo)
+        boseLogo.translatesAutoresizingMaskIntoConstraints = false
+//        boseLogo.contentMode = .scaleAspectFit
+        let top = NSLayoutConstraint(item: boseLogo, attribute: .topMargin, relatedBy: .equal, toItem: view, attribute: .topMargin, multiplier: 1, constant: 0)
+        let leading = NSLayoutConstraint(item: boseLogo, attribute: .leading, relatedBy: .equal, toItem: view, attribute: .leading , multiplier: 1, constant: 0)
+        let trailing = NSLayoutConstraint(item: boseLogo, attribute: .trailing, relatedBy: .equal, toItem: view, attribute: .trailing, multiplier: 1, constant: 0)
+        let height = NSLayoutConstraint(item: boseLogo, attribute: .height, relatedBy: .equal, toItem: view, attribute: .height, multiplier: 0.20, constant: 0)
+        view.addConstraints([top,leading,trailing,height])
+    }
+    
+    private func setupButtonsStackView() {
+        setupButtons()
+        buttonsStackView = UIStackView(arrangedSubviews: [playButton,recordButton])
+        view.addSubview(buttonsStackView)
+        buttonsStackView.translatesAutoresizingMaskIntoConstraints = false
+        buttonsStackView.axis = .horizontal
+        buttonsStackView.spacing = 25
+        buttonsStackView.distribution = .fill
+        let centerX = NSLayoutConstraint(item: buttonsStackView, attribute: .centerX, relatedBy: .equal, toItem: view, attribute: .centerX, multiplier: 1, constant: 0)
+        let centerY = NSLayoutConstraint(item: buttonsStackView, attribute: .centerY, relatedBy: .equal, toItem: view, attribute: .centerY, multiplier: 1, constant: 0)
+        view.addConstraints([centerX,centerY])
     }
 
     private func setupButtons() {
@@ -68,18 +80,18 @@ class ViewController: UIViewController {
         playButton.isEnabled = false
         setHeightAndWidth(for: playButton)
         setHeightAndWidth(for: recordButton)
-        playButton.layer.cornerRadius = 12
-        recordButton.layer.cornerRadius = 12
+        playButton.layer.cornerRadius = 5
+        recordButton.layer.cornerRadius = 5
         playButton.clipsToBounds = true
         recordButton.clipsToBounds = true
         playButton.setTitle("Play", for: .normal)
         playButton.setTitle("Pause", for: .selected)
         recordButton.setTitle("Record", for: .normal)
         recordButton.setTitle("Stop Recording", for: .selected)
-        playButton.setTitleColor(.black, for: .normal)
-        recordButton.setTitleColor(.black, for: .normal)
-        playButton.backgroundColor = .green
-        recordButton.backgroundColor = .red
+        playButton.setTitleColor(.white, for: .normal)
+        recordButton.setTitleColor(.white, for: .normal)
+        playButton.backgroundColor = butttonBackgroundColor
+        recordButton.backgroundColor = butttonBackgroundColor
         playButton.translatesAutoresizingMaskIntoConstraints = false
         recordButton.translatesAutoresizingMaskIntoConstraints = false
         setHeightAndWidth(for: playButton)
@@ -110,8 +122,8 @@ class ViewController: UIViewController {
     }
     
     private func setHeightAndWidth(for button: UIButton) {
-        let height = NSLayoutConstraint(item: button, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 25)
-        let width = NSLayoutConstraint(item: button, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 125)
+        let height = NSLayoutConstraint(item: button, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 30)
+        let width = NSLayoutConstraint(item: button, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 120)
         view.addConstraints([height,width])
     }
 }
@@ -134,27 +146,32 @@ extension ViewController: AVAudioRecorderDelegate {
             print("Could not setup audio recorder - \(error)")
         }
     }
-    private func getRecordingURL() -> URL {
-        let fileManager = FileManager.default
-        let documentsURL = try! fileManager.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
-        return documentsURL.appendingPathComponent(audioRecordingFileName)
-    }
 }
 
 extension ViewController: AVAudioPlayerDelegate {
     private func audioPlayer(play: Bool) {
         if play {
             do {
-                audioPlayer = try AVAudioPlayer(contentsOf: getRecordingURL())
+                guard let recordingURL = getRecordingURL() else { return }
+                audioPlayer = try AVAudioPlayer(contentsOf: recordingURL)
+                audioPlayer.numberOfLoops = -1
+                audioPlayer.play()
             } catch let error {
-                print("Error creating audio player - \(error)")
+                print("Error creating AVAudioPlayer - \(error)")
             }
-            audioPlayer.numberOfLoops = -1
-            audioPlayer.play()
-            print("Playing audio")
         } else {
             audioPlayer.pause()
-            print("Audio paused")
         }
+    }
+    
+    private func getRecordingURL() -> URL? {
+        let fileManager = FileManager.default
+        do {
+            let documentsURL = try fileManager.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
+            return documentsURL.appendingPathComponent(audioRecordingFileName)
+        } catch let error {
+            print("Error creating url to document directory - \(error)")
+        }
+        return nil
     }
 }
